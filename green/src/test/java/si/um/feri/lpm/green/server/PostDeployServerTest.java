18c988da9b4d92881a4c0eb0132cd7e5c623e6e8
Žiga Leber <zigaleber7@gmail.com>
2022-11-07 19:29:18 +0100

    Add post deploy tests

---
+++ green/src/test/java/si/um/feri/lpm/green/server/PostDeployServerTest.java
@@ -0,0 +1,33 @@
+package si.um.feri.lpm.green.server;
+
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.Tag;
+import si.um.feri.lpm.green.grpc.sunflow.SunflowKnobs;
+import si.um.feri.lpm.green.grpc.sunflow.SunflowServiceGrpc;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostDeployServerTest {
+
+    @Tag("PostDeploy")
+    @org.junit.jupiter.api.Test
+    void testSunflow() throws IOException {
+        var request = SunflowKnobs.newBuilder()
+                .setThreads(1)
+                .setResolution(64)
+                .setAaMin(-1)
+                .setAaMax(1)
+                .build();
+
+        var channel = ManagedChannelBuilder.forTarget("localhost:50051")
+                .usePlaintext()
+                .build();
+
+        var stub = SunflowServiceGrpc.newBlockingStub(channel);
+        var response = stub.fitness(request);
+        System.out.print(response.getEnergy());
+        assertTrue(response.getEnergy() > 0.0);
+    }
+}
