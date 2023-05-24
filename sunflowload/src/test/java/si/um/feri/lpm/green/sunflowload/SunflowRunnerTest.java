eb28ec99bc6bfd02eab4f292f299426610bc0126
Žiga Leber <zigaleber7@gmail.com>
2023-05-24 15:29:57 +0200

    Fix wrong parameters in a test

--- sunflowload/src/test/java/si/um/feri/lpm/green/sunflowload/SunflowRunnerTest.java
+++ sunflowload/src/test/java/si/um/feri/lpm/green/sunflowload/SunflowRunnerTest.java
@@ -17,8 +17,10 @@ class SunflowRunnerTest {
     @Test
     void measureDistanceDefault() throws InterruptedException {
         final var factory = new SunflowRunnerFactory();
-        final var runner = factory.new Runner(new SunflowKnobs((int)6.700394102746045, (int)116.77794291077436, (int)-2.5658260699149356, (int)-1.5169675399851819));
+        final var runner = factory.new Runner(SunflowKnobs.DEFAULT);
         runner.run();
         assertTrue(runner.measureDistance() > 0.0);
     }
+
+    //final var runner = factory.new Runner(new SunflowKnobs((int)6.700394102746045, (int)116.77794291077436, (int)-2.5658260699149356, (int)-1.5169675399851819));
 }
