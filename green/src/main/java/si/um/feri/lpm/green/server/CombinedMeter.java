eefbcc08ee4439fc81208ba7345cf8fff14868b2
Žiga Leber <zigaleber7@gmail.com>
2023-06-07 14:19:33 +0200

    Accumulate samples in batches

--- green/src/main/java/si/um/feri/lpm/green/server/CombinedMeter.java
+++ green/src/main/java/si/um/feri/lpm/green/server/CombinedMeter.java
@@ -5,8 +5,48 @@ import eflect.data.EnergyFootprint;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.logging.Logger;
 
+class Accumulator implements Runnable {
+    private static final long WAIT_TIME_MS = 60000;
+
+    private double energy;
+    private int samplesCount;
+
+    public Accumulator() {
+        this.energy = 0;
+        this.samplesCount = 0;
+    }
+
+    public void update() {
+        final var samples = Eflect.getInstance().read();
+        samplesCount += samples.size();
+        for (EnergyFootprint footprint : samples) {
+            energy += footprint.energy;
+        }
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.interrupted()) {
+            try {
+                Thread.sleep(WAIT_TIME_MS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            Eflect.getInstance().stop();
+            update();
+            Eflect.getInstance().start();
+        }
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public int getSamplesCount() {
+        return samplesCount;
+    }
+}
 
 public class CombinedMeter implements Meter {
 
@@ -17,27 +57,31 @@ public class CombinedMeter implements Meter {
                 Runtime.getRuntime().maxMemory(),
                 Runtime.getRuntime().freeMemory()));
 
+        final var accumulator = new Accumulator();
         Eflect.getInstance().start(4);
+        Thread t = new Thread(accumulator);
         Instant start = Instant.now();
         runnable.run();
         Instant end = Instant.now();
+        t.interrupt();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Eflect.getInstance().stop();
+        accumulator.update();
+
         GreenLogger.get().info(String.format("green: stop thread %s gc total %d max %d free %d",
                 Thread.currentThread().getName(),
                 Runtime.getRuntime().totalMemory(),
                 Runtime.getRuntime().maxMemory(),
                 Runtime.getRuntime().freeMemory()));
 
-        final var samples = Eflect.getInstance().read();
-        double energy = 0;
-        for (EnergyFootprint footprint : samples) {
-            energy += footprint.energy;
-        }
-
         long time = Duration.between(start, end).toMillis();
 
-        GreenLogger.get().info(String.format("green: energy %f samples %d time %d", energy, samples.size(), time));
+        GreenLogger.get().info(String.format("green: energy %f samples %d time %d", accumulator.getEnergy(), accumulator.getSamplesCount(), time));
 
-        return new Measurements(energy, time);
+        return new Measurements(accumulator.getEnergy(), time);
     }
 }
